d7f63de0b4dfdbb128006d716384aaf409d22f6f
唐晓龙 <txlong_onz@163.com>
2016-05-18 00:43:53 +0800

    使用心得试验中gradle插件进行修改

--- app/src/main/jni/hello-jni.c
+++ app/src/main/jni/hello-jni.c
@@ -16,6 +16,7 @@
  */
 #include <string.h>
 #include <jni.h>
+#include <android/log.h>
 
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
@@ -26,37 +27,38 @@
 jstring
 Java_com_ianonymous_androidjni_HelloJni_stringFromJNI(JNIEnv *env, jobject thiz) {
 #if defined(__arm__)
-#if defined(__ARM_ARCH_7A__)
-#if defined(__ARM_NEON__)
-#if defined(__ARM_PCS_VFP)
-#define ABI "armeabi-v7a/NEON (hard-float)"
-#else
-#define ABI "armeabi-v7a/NEON"
-#endif
-#else
-#if defined(__ARM_PCS_VFP)
-#define ABI "armeabi-v7a (hard-float)"
-#else
-#define ABI "armeabi-v7a"
-#endif
-#endif
-#else
-#define ABI "armeabi"
-#endif
+    #if defined(__ARM_ARCH_7A__)
+        #if defined(__ARM_NEON__)
+            #if defined(__ARM_PCS_VFP)
+                #define ABI "armeabi-v7a/NEON (hard-float)"
+            #else
+                #define ABI "armeabi-v7a/NEON"
+            #endif
+        #else
+            #if defined(__ARM_PCS_VFP)
+                #define ABI "armeabi-v7a (hard-float)"
+            #else
+                #define ABI "armeabi-v7a"
+            #endif
+        #endif
+    #else
+        #define ABI "armeabi"
+    #endif
 #elif defined(__i386__)
-#define ABI "x86"
+    #define ABI "x86"
 #elif defined(__x86_64__)
-#define ABI "x86_64"
+    #define ABI "x86_64"
 #elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
-#define ABI "mips64"
+    #define ABI "mips64"
 #elif defined(__mips__)
-#define ABI "mips"
+    #define ABI "mips"
 #elif defined(__aarch64__)
-#define ABI "arm64-v8a"
+    #define ABI "arm64-v8a"
 #else
-#define ABI "unknown"
+    #define ABI "unknown"
 #endif
 
+    __android_log_print(ANDROID_LOG_ERROR, "tag", "msg");
     return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI " ABI ".");
 }
 
