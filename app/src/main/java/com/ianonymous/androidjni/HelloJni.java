49188cc824f67abaa43d543da61f6ed57b7591b4
iAnonymous <txlong_onz@163.com>
2016-05-18 16:10:29 +0800

    添加C端aes加密算法

--- app/src/main/java/com/ianonymous/androidjni/HelloJni.java
+++ app/src/main/java/com/ianonymous/androidjni/HelloJni.java
@@ -16,10 +16,17 @@
 package com.ianonymous.androidjni;
 
 import android.app.Activity;
-import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ianonymous.androidjni.aes.AESCryptor;
+import com.ianonymous.androidjni.aes.JavaAESCryptor;
+
+import java.io.UnsupportedEncodingException;
 
 public class HelloJni extends Activity {
+    public static final String TESTDATA = "kLNyk5O9jj0kG/lqskHCLs7HQttQjqMwNToSXGVs7WraXf0bVpBA1vaE+30Mz2wu/6dmmU6mHOVAye+w9zrgZswPAjqEtU8nAa7+z5RDeil/5kBEEnV/IVO+Xry6YO4AL6xuHm/6k32zn6C8R2ZCvNL/vvUbk49YH5MEyCU/9See8Y8hqM9jPTmGc9+izcIjZtkMnC1PfShwvgdtE5gkkBqVJx20bnjyzEEPIb3dxt/DlhmbnpBeC6GWzCHjzvdLcC3mfYHoP6+A1r+oXjDxGFfKIDgtwaUZfzAKhlpsx9gOn7e2CaC85Nyu2Xy1vjTBlJiwN1EPvI87nQrWWqOBDyRRzhlbc+f2pEfZ6yIQKXnR7QKLKptxnD3jcKuH5r2l82b1Q3OSFTCYRCzYtA/CYbdJq4gRxx8bFwSeqmxtYy0=kLNyk5O9jj0kG/lqskHCLs7HQttQjqMwNToSXGVs7WraXf0bVpBA1vaE+30Mz2wu/6dmmU6mHOVAye+w9zrgZswPAjqEtU8nAa7+z5RDeil/5kBEEnV/IVO+Xry6YO4AL6xuHm/6k32zn6C8R2ZCvNL/vvUbk49YH5MEyCU/9See8Y8hqM9jPTmGc9+izcIjZtkMnC1PfShwvgdtE5gkkBqVJx20bnjyzEEPIb3dxt/DlhmbnpBeC6GWzCHjzvdLcC3mfYHoP6+A1r+oXjDxGFfKIDgtwaUZfzAKhlpsx9gOn7e2CaC85Nyu2Xy1vjTBlJiwN1EPvI87nQrWWqOBDyRRzhlbc+f2pEfZ6yIQKXnR7QKLKptxnD3jcKuH5r2l82b1Q3OSFTCYRCzYtA/CYbdJq4gRxx8bFwSeqmxtYy0=";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,36 @@ public class HelloJni extends Activity {
         TextView tv = new TextView(this);
         tv.setText(stringFromJNI());
         setContentView(tv);
+
+        try {
+            System.out.println("======jni-crypt-test======");
+            byte[] data = TESTDATA.getBytes("UTF-8");
+            data = AESCryptor.crypt(data, System.currentTimeMillis(), 0);
+            String hexStr = AESCryptor.bytes2HexStr(data);
+            System.out.println("encrypt:" + hexStr);
+
+            data = AESCryptor.hexStr2Bytes(hexStr);
+            data = AESCryptor.crypt(data, System.currentTimeMillis(), 1);
+            System.out.println("decrypt:" + new String(data, "UTF-8"));
+
+            System.out.println("======java-crypt-test======");
+            data = TESTDATA.getBytes("UTF-8");
+            data = JavaAESCryptor.encrypt(data);
+            hexStr = AESCryptor.bytes2HexStr(data);
+            System.out.println("encrypt:" + hexStr);
+
+            data = AESCryptor.hexStr2Bytes(hexStr);
+            data = JavaAESCryptor.decrypt(data);
+            System.out.println("decrypt:" + new String(data, "UTF-8"));
+
+            System.out.println("======jni-file-test======");
+            data = AESCryptor.read("/mnt/sdcard/test.txt", System.currentTimeMillis());
+            if (data != null) {
+                System.out.println("read:" + new String(data, "UTF-8"));
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
     /* A native method that is implemented by the
@@ -57,6 +94,6 @@ public class HelloJni extends Activity {
      * installation time by the package manager.
      */
     static {
-        System.loadLibrary("MyJni");
+        System.loadLibrary("aes_jni");
     }
 }
